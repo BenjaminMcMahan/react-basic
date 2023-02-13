c42f6cce10dd0094c0f003f0b0b3312c450611a7
Benjamin McMahan <108589431+BenjaminMcMahan@users.noreply.github.com>
2023-02-12 19:49:26 -0500

    Begins the implementation of Passport

--- server/server.js
+++ server/server.js
@@ -1,13 +1,34 @@
 const express = require('express');
-const mongoose = require('./db')
+const mongoose = require('./db');
+const passport = require("passport");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
 const cors = require('cors');
+require('dotenv').config();
 
+// Initialize Express App
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+// CORS setup
+app.use(cors({origin: 'http://localhost:3000', credentials: true}));
 
+// Initialize connection to Mongoose Atlas
 const connection = mongoose.connection;
 
+// JWT options for Passport
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.SECRET_KEY
+};
+passport.use(
+    new JwtStrategy(jwtOptions, (jwt_payload, done) => {
+        // Your authentication logic goes here
+        // If the authentication is successful, call done(null, user)
+        // If the authentication fails, call done(null, false)
+    })
+);
+
+app.use(passport.initialize({}));
 connection.once('open', () => {
     console.log('Mongoose connection established successfully!');
 });
@@ -17,7 +38,7 @@ connection.on('error', (error) => {
 });
 
 app.get('/', (req, res) => {
-    res.send({ data: 'Hello from the backend!' });
+    res.send({data: 'Hello from the backend!'});
 });
 
 app.listen(3001, () => {
